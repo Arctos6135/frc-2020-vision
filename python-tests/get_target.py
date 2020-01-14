5244d5e3631daf7ebe3f2ae80a141912357d7853
Tyler Tian <tylertian123@gmail.com>
2020-01-13 19:09:14 -0500

    Add Python test code for target finding
    Add Python code that can filter out the target from an image, giving its
    bounding box and contours.

--- python-tests/get_target.py
+++ python-tests/get_target.py
@@ -10,6 +10,14 @@ MORPH_KERNEL_SIZE = 5
 
 MORPH_KERNEL = cv2.getStructuringElement(cv2.MORPH_RECT, (MORPH_KERNEL_SIZE, MORPH_KERNEL_SIZE))
 
+CAM_HORIZ_FOV = 61
+CAM_VERT_FOV = 37
+CAM_WIDTH = 1280
+CAM_HEIGHT = 720
+
+FILTER_FULLNESS_HIGH = 0.25
+FILTER_FULLNESS_LOW = 0.10
+
 def preprocess(img):
     """
     Pre-process an image.
@@ -30,3 +38,59 @@ def preprocess(img):
     img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, MORPH_KERNEL)
     return img
 
+
+def filter_targets(targets):
+    """
+    Filter a list of possible targets to find the correct one.
+
+    If the input list is empty, or no target meets the requirement, this
+    function will return None.
+    """
+    if not targets:
+        return None
+    valid_targets = []
+    for contour, rect in targets:
+        # RotatedRect format: 
+        rect_area = rect[1][0] * rect[1][1]
+        if rect_area == 0:
+            continue
+        area = cv2.contourArea(contour)
+        # Filter by fullness
+        # Fullness is the percentage of the bounding box filled by the contour
+        fullness = area / rect_area
+        if FILTER_FULLNESS_LOW <= fullness <= FILTER_FULLNESS_HIGH:
+            print(f"Contour meets fullness requirement: area={area}, rect_area={rect_area}")
+            valid_targets.append(((contour, rect), area))
+        else:
+            print(f"Contour does not meet fullness requirement: area={area}, rect_area={rect_area}")
+    # If there are multiple left, return the one with the biggest area
+    target = max(valid_targets, key=lambda v: v[1], default=None)
+    return target[0] if target else None
+
+
+def get_target(img):
+    """
+    Find the target from a pre-processed image.
+    
+    Returns a tuple of (contour, rect) where contour is the OpenCV contour of
+    the target, and rect is the minimum bounding rect (rotated rect).
+
+    Returns None if no target is found.
+    """
+    # Find the contours
+    # We only care about external contours
+    contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # Get the minimum bounding box of each target and filter
+    target = filter_targets([(contour, cv2.minAreaRect(contour)) for contour in contours])
+    return target
+
+
+def draw_target(img, target):
+    """
+    Draw a target's contour and bonding box onto an image.
+    
+    Source: https://stackoverflow.com/a/18208020/5745575
+    """
+    box = np.int0(cv2.boxPoints(target[1]))
+    cv2.drawContours(img, [target[0]], -1, (0, 0, 255), 3)
+    cv2.drawContours(img, [box], -1, (0, 255, 255), 2)
--- python-tests/vision_test.py
+++ python-tests/vision_test.py
@@ -3,18 +3,37 @@ Live vision tests.
 """
 import cv2
 import numpy as np
+import time
 from get_target import *
 
 cam = cv2.VideoCapture(1)
+
+# Configure camera
+print("Configuring Camera")
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, CAM_WIDTH)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, CAM_HEIGHT)
+# 1 means off for some reason
+cam.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
+time.sleep(2)
+cam.set(cv2.CAP_PROP_EXPOSURE, 10)
+time.sleep(0.5)
 
 while True:
     ret, frame = cam.read()
     if not ret:
         print("Capture failed!")
         break
-    img = preprocess(img)
 
-    cv2.imshow("Image Output", img)
+    out = preprocess(frame)
+    target = get_target(out)
+    if target is not None:
+        draw_target(frame, target)
+        # Convert the pre-processed image to BGR so the contour can be shown
+        out = cv2.cvtColor(out, cv2.COLOR_GRAY2BGR)
+        draw_target(out, target)
+
+    cv2.imshow("Raw Image", frame)
+    cv2.imshow("Pre-processed Image", out)
     
     code = cv2.waitKey(50) & 0xFF
     if code == ord('q'):
