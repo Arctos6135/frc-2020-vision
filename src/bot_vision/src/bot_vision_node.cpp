86f8e314bd9697c70c637f93a7e5eb0bc390b01d
Tyler Tian <tylertian123@gmail.com>
2020-01-20 17:11:47 -0500

    Add dynamic_reconfigure setup

--- src/bot_vision/src/bot_vision_node.cpp
+++ src/bot_vision/src/bot_vision_node.cpp
@@ -1,13 +1,23 @@
 #include <ros/ros.h>
+
 #include "bot_vision/vision.h"
-#include <iostream>
+#include <dynamic_reconfigure/server.h>
+#include "bot_vision/VisionConfig.h"
+
+void configure_callback(bot_vision::VisionConfig &config, uint32_t level) {
+    
+}
 
 int main(int argc, char *argv[]) {
     // Init ROS
     ros::init(argc, argv, "bot_vision_node");
     // Get handle to current node
     ros::NodeHandle handle("~");
-    std::cout << "Hello world" << std::endl;
+
+    // Set up dynamic_reconfigure
+    dynamic_reconfigure::Server<bot_vision::VisionConfig> server;
+    server.setCallback(configure_callback);
+    
     ros::spin();
 
     return 0;
